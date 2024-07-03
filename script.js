ae171a3b2cdfedc037c99aeaa575170ef0166d5d
TheUglyAlpaca <91501695+TheUglyAlpaca@users.noreply.github.com>
2024-07-03 04:04:20 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,78 +1,94 @@
 let sheetData = null;
 let sortMode = null;
 
-
-backgrounds = [
+const backgrounds = [
     'url("photo1.jpg")',
     'url("photo2.jpg")',
     'url("photo4.jpg")',
     'url("photo5.jpg")',
     'url("photo6.jpg")',
-]
+];
 
 function randomizeBackground() {
-    selectedImage = backgrounds[Math.floor(Math.random() * backgrounds.length)];
+    const selectedImage = backgrounds[Math.floor(Math.random() * backgrounds.length)];
     document.body.style.background = selectedImage;
     document.body.style.backgroundRepeat = "no-repeat";
     document.body.style.backgroundSize = "cover";
 }
 
+// Shuffle function to randomize array elements
+function shuffle(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+}
+
 // fetch data from google sheet
 fetch("https://docs.google.com/spreadsheets/d/e/2PACX-1vSJCkQKlmRxz6qgU4o4tbHkwx9qPEYECdslflOytiguegF2IGnxvDHrD-Qqtuy6bewspfMbYS-p1ntf/pub?output=tsv")
-    .then(res => res.text()).then(csv => {
-        // csv parsing lets fucking GOOO
+    .then(res => res.text())
+    .then(csv => {
+        // csv parsing
         sheetData = csv.replace(/"|\r/g, "") // remove quotation marks and whatever \r is
             .split("\n") // split at each line
-            .map(x => x.split("\t")) // split each line at each tab (its actually tsv not csv lmao)
+            .map(x => x.split("\t")); // split each line at each tab (its actually tsv not csv lmao)
         console.table(sheetData);
 
         function getColumn(table, column) {
             return table.map(x => x[column]);
         }
 
-        //Camper Names
-        let names = getColumn(sheetData, 0).slice(1); //cuts off first index of names array (header text)
-        names.sort();
+        // Camper Data
+        let campers = sheetData.slice(1).map(row => ({
+            name: row[0],
+            unit: row[2],
+            gender: row[3]
+        }));
 
-        //Camper Units (Rookie or Post)
-        let units = getColumn(sheetData, 2).slice(1);
+        // Shuffle the campers array to randomize names, genders, and units together
+        shuffle(campers);
 
+        // Sort the campers array by name to ensure alphabetical order
+        campers.sort((a, b) => a.name.localeCompare(b.name));
+
+        // Separate the randomized data into individual arrays
+        let names = campers.map(camper => camper.name);
+        let units = campers.map(camper => camper.unit);
+        let genders = campers.map(camper => camper.gender);
+
+        // Camper Units (Rookie or Post)
         let rookies = [];
         let posts = [];
 
         for (let i = 0; i < names.length; i++) {
-            if (units[i] == ("Rookie")) {
+            if (units[i] === "Rookie") {
                 rookies.push(names[i]);
-            }
-            else {
+            } else {
                 posts.push(names[i]);
             }
         }
 
-        //Camper Units (Male or Female)
-        let genders = getColumn(sheetData, 3).slice(1);
-
+        // Camper Units (Male or Female)
         let males = [];
         let females = [];
 
         for (let i = 0; i < names.length; i++) {
-            if (genders[i] == ("male")) {
+            if (genders[i] === "male") {
                 males.push(names[i]);
-            }
-            else {
+            } else {
                 females.push(names[i]);
             }
         }
 
-        //Checked For Sorting
+        // Checked For Sorting
         let checkedSort = names;
 
         function addNames(data) {
             const camperList = $("#camperlist");
             data.forEach(camper => {
-                let camperString = `<div class="camper"><label class="switch" id="activateButton"><input checked class="camperCheck" type="checkbox" camper="${camper}"><span class="slider round"></span></label><p>${camper}</p></div>`
+                let camperString = `<div class="camper"><label class="switch" id="activateButton"><input checked class="camperCheck" type="checkbox" camper="${camper}"><span class="slider round"></span></label><p>${camper}</p></div>`;
                 camperList.append(camperString);
-            })
+            });
             console.log(checkedSort);
         }
 
@@ -80,21 +96,21 @@ fetch("https://docs.google.com/spreadsheets/d/e/2PACX-1vSJCkQKlmRxz6qgU4o4tbHkwx
 
         document.getElementById("otherSorts").style.visibility = 'hidden';
 
-        //clicking name also toggles checkbox
+        // clicking name also toggles checkbox
         $(".camper p").click(function () {
-            $(this).parent().find("input").trigger("click")
+            $(this).parent().find("input").trigger("click");
             camperName = $(this).text();
-        })
+        });
 
         $(".camperCheck").on("input", updateNameCounts);
 
         function updateNameCounts() {
-            //displays number of unchecked campers
+            // displays number of unchecked campers
             let totalUnselected = $(".camperCheck:not(:checked)");
-            $("#totalUnselected").text(totalUnselected.length)
+            $("#totalUnselected").text(totalUnselected.length);
 
-            //displays number of unchecked campers as a ratio
-            $("#ratio").text(`${names.length - totalUnselected.length}/${names.length}`)
+            // displays number of unchecked campers as a ratio
+            $("#ratio").text(`${names.length - totalUnselected.length}/${names.length}`);
         }
 
         $(".sortBtn").click(function () {
@@ -102,148 +118,139 @@ fetch("https://docs.google.com/spreadsheets/d/e/2PACX-1vSJCkQKlmRxz6qgU4o4tbHkwx
             $(this).addClass("selectedSort");
 
             sortMode = $(this).attr("sortMode");
-        })
+        });
 
         $(".deselectrooks").click(function () {
-            // Check the current state of the first rookie checkbox to determine the action
-            let shouldDeselect = rookies.every(rookie => $(`.camperCheck[camper="${rookie}"]`).is(":checked"));
-        
-            // Toggle the selection state based on the current state
+            // Check if any rookies are currently selected
+            let anySelected = rookies.some(rookie => $(`.camperCheck[camper="${rookie}"]`).is(":checked"));
+
+            // Toggle the selection state based on whether any rookies are selected
             rookies.forEach(rookie => {
-                $(`.camperCheck[camper="${rookie}"]`).prop("checked", !shouldDeselect);
+                $(`.camperCheck[camper="${rookie}"]`).prop("checked", !anySelected);
             });
-        
+
             // Update the counts after toggling
             updateNameCounts();
         });
-        
+
         $(".deselectposts").click(function () {
-            // Check the current state of the first post checkbox to determine the action
-            let shouldDeselect = posts.every(post => $(`.camperCheck[camper="${post}"]`).is(":checked"));
-        
-            // Toggle the selection state based on the current state
+            // Check if any posts are currently selected
+            let anySelected = posts.some(post => $(`.camperCheck[camper="${post}"]`).is(":checked"));
+
+            // Toggle the selection state based on whether any posts are selected
             posts.forEach(post => {
-                $(`.camperCheck[camper="${post}"]`).prop("checked", !shouldDeselect);
+                $(`.camperCheck[camper="${post}"]`).prop("checked", !anySelected);
             });
-        
+
             // Update the counts after toggling
             updateNameCounts();
         });
-        
-        
 
-
-        //CLICK BUTTON
+        // CLICK BUTTON
         $("#createGroupsButton").click(function () {
-
-            let numGroups = +$("#quantity").val()
-            let programName = $("#name").val()
+            let numGroups = +$("#quantity").val();
+            let programName = $("#name").val();
 
             document.getElementById("sortedCampers").style.display = "flex";
 
-            //checks for empty values
-            if (!numGroups) return alert("Please select the number of groups to sort.")
-            if (numGroups > 20) return alert("Please select a number between 2 and 20")
-            else if (!sortMode) return alert("Please select the sorting method.")
+            // checks for empty values
+            if (!numGroups) return alert("Please select the number of groups to sort.");
+            if (numGroups > 20) return alert("Please select a number between 2 and 20");
+            else if (!sortMode) return alert("Please select the sorting method.");
 
-            //returns an array of all selected campers (camper attribute for selected campers)
+            // returns an array of all selected campers (camper attribute for selected campers)
             let checkedCampers = [];
             $(".camperCheck:checked").each(function () {
-                return checkedCampers.push($(this).attr("camper"))
-            })
+                return checkedCampers.push($(this).attr("camper"));
+            });
 
-            //Sorting rookies and posts into two different arrays
+            // Sorting rookies and posts into two different arrays
             let checkedRookies = [];
             let checkedPosts = [];
 
             for (let i = 0; i < rookies.length; i++) {
                 for (let j = 0; j < checkedCampers.length; j++) {
-                    if (rookies[i] == checkedCampers[j]) {
-                        checkedRookies.push(rookies[i])
+                    if (rookies[i] === checkedCampers[j]) {
+                        checkedRookies.push(rookies[i]);
                     }
                 }
             }
 
             for (let i = 0; i < posts.length; i++) {
                 for (let j = 0; j < checkedCampers.length; j++) {
-                    if (posts[i] == checkedCampers[j]) {
-                        checkedPosts.push(posts[i])
+                    if (posts[i] === checkedCampers[j]) {
+                        checkedPosts.push(posts[i]);
                     }
                 }
             }
 
-            //Sorting males and females into two different arrays
+            // Sorting males and females into two different arrays
             let checkedMales = [];
             let checkedFemales = [];
 
             for (let i = 0; i < males.length; i++) {
                 for (let j = 0; j < checkedCampers.length; j++) {
-                    if (males[i] == checkedCampers[j]) {
-                        checkedMales.push(males[i])
+                    if (males[i] === checkedCampers[j]) {
+                        checkedMales.push(males[i]);
                     }
                 }
             }
 
             for (let i = 0; i < females.length; i++) {
                 for (let j = 0; j < checkedCampers.length; j++) {
-                    if (females[i] == checkedCampers[j]) {
-                        checkedFemales.push(females[i])
+                    if (females[i] === checkedCampers[j]) {
+                        checkedFemales.push(females[i]);
                     }
                 }
             }
 
-            //Generic 
+            // Generic
             let genericScore = getColumn(sheetData, 4).slice(1);
 
-            bestToWorst = [];
-            temporarySort = [];
+            let bestToWorst = [];
+            let temporarySort = [];
 
-            //sorts highest to lowest
+            // sorts highest to lowest
             for (let i = 4; i > 0; i--) {
                 for (let j = 0; j < genericScore.length; j++) {
                     if (genericScore[j] == i) {
                         temporarySort.push(names[j]);
-
                     }
                 }
-                randomize(temporarySort);
+                shuffle(temporarySort);
                 temporarySort.forEach(camper => {
-                    bestToWorst.push(camper)
-                })
-                temporarySort = []
+                    bestToWorst.push(camper);
+                });
+                temporarySort = [];
             }
 
-            checkedGeneric = [];
+            let checkedGeneric = [];
 
             for (let i = 0; i < bestToWorst.length; i++) {
                 for (let j = 0; j < checkedCampers.length; j++) {
-                    if (bestToWorst[i] == checkedCampers[j]) {
-                        checkedGeneric.push(bestToWorst[i])
+                    if (bestToWorst[i] === checkedCampers[j]) {
+                        checkedGeneric.push(bestToWorst[i]);
                     }
                 }
             }
 
-            //SORTS BASED ON SELECTED SORT METHOD
+            // SORTS BASED ON SELECTED SORT METHOD
             switch (sortMode) {
                 case "trueRandom":
                     trueRandom(numGroups, checkedCampers, programName);
                     break;
-
                 case "evenRookiePostSort":
                     evenRookiePostSort(numGroups, checkedRookies, checkedPosts, programName);
                     break;
-
                 case "evenGendersSort":
                     evenGendersSort(numGroups, checkedMales, checkedFemales, programName);
                     break;
-
-                case "genericSort": //wip
+                case "genericSort":
                     genericSort(numGroups, checkedGeneric, programName);
                     break;
             }
-        })
+        });
 
         randomizeBackground();
         updateNameCounts();
-
-    })
+    });
